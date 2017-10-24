c1cfac384b5bc7654886904923f687dd684fa49b
Souad360 <32779871+Souad360@users.noreply.github.com>
2017-10-24 15:41:39 +0200

    Update Tail_DiffException.java

--- Tail_DiffException.java
+++ Tail_DiffException.java
@@ -1,5 +1,15 @@
 /**
- * Created by Yacine on 10/24/2017.
+ * Created by Yacine and Souad on 10/24/2017.
  */
 public class Tail_DiffException extends Exception{
+  
+    private static final long serialVersionUID = 1L;
+
+    public void tailleDiff(){
+
+        System.out.println("la taille des deux tableaux est différente  , on peux pas les sommer !!!! ");
+    }
+
+  
+  
 }
