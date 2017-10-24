c3fa580456318dc7ed6ba5cd54f655bf84a8e36b
yacine06 <33061201+yacine06@users.noreply.github.com>
2017-10-24 22:35:03 +0100

    Update VectorHelper.java

--- VectorHelper.java
+++ VectorHelper.java
@@ -1,12 +1,14 @@
 import java.util.ArrayList;
-/**@author yacine and souad
-*@version 3.0
+/** *********TP N_01 IGL Application 2 VectorHelper *******************
+ * Morakeb Yacine -- Aboud Souad
+ * Groupe 10
+ * Email: fy_morakeb@esi.dz / fs_aboud@esi.dz
+ * @author yacine
+ *@version 3.0
  * Created by Yacine on 10/18/2017.
  */
 public class VectorHelper {
-   
-    private ArrayList<Double> vecteur = new ArrayList<Double>(); 
-   
+    
     /**
      * un constreucteur par defaut de la class VectorHelper*/
     public void VectorHelper() {
@@ -14,21 +16,22 @@ public class VectorHelper {
     }
 
     /**
-     * cette fonction permet de trié les éléments d'un Vecteur et return on sorter un vecteur trié celon l'ordre croissant
+     * cette fonction permet de trie les elements d'un Vecteur et return on sorter un vecteur trie celon l'ordre croissant
      *
      * @param vec c'est un vecteur de type double dooner en entrer de la fonction
-     * @return le fonction return le meme vecteur donner en entrer mais trié
+     * @return la fonction return le meme vecteur donner en entrer mais trie en ordre croissant
      */
 
     public  ArrayList<Double> SortVec(ArrayList<Double> vec ){
-        /**on a utiliser un trié par bulles */
+        /**
+         * on a utiliser un trie par bulles */
         for (int i=0;i<vec.size();i++)
         {
 
             for (int j=0;j<vec.size()-1;j++){
-              /**on test et on permute on utilisons une variable de sauvgarde */
+                /**on test et on permute on utilisons une variable de sauvgarde */
                 if (vec.get(j)>vec.get(j+1)){
-                   /**x une variable pour le sauvgarde*/
+                    /**x une variable pour le sauvgarde*/
                     Double x;
                     x=vec.get(j);
                     vec.set(j,vec.get(j+1));
@@ -41,39 +44,39 @@ public class VectorHelper {
         return vec;
     }
     /**
-     * cette fonction permet d'appliquer une formule mathématiques pour touts les éléments de Vecteur
-      *@since 3.0
-        *@param vec c'est un vecteur de type double
-        * @param f la formule a appliquer
-        * @param n les données de la formule
-        *@return
+     * cette fonction permet d'appliquer une formule mathematiques pour touts les elements de Vecteur
+     *@since 3.0
+     *@param vec c'est un vecteur de type double
+     * @param f la formule a appliquer
+     * @param n les donnees de la formule
+     *@return
 
-        le fonction return le meme vecteur donner en entrer on appliquns la formule donner en entrer a touts les éléments de vecteur*/
+    le fonction return le meme vecteur donner en entrer on appliquns la formule donner en entrer a touts les elements de vecteur*/
     public  ArrayList<Double> VecForm(ArrayList<Double> vec, String f,int n){
-        /**une fonction qui permet dappliquer une formule a toutes les éléments de Vecteur*/
-       if(f.equals("+")){
+        /**une fonction qui permet dappliquer une formule a toutes les elements de Vecteur*/
+        if(f.equals("+")){
 
-         /**l'addition*/
-        for (int i=0;i<vec.size();i++) {
-           vec.set(i,(vec.get(i)+n)) ;
+            /**l'addition*/
+            for (int i=0;i<vec.size();i++) {
+                vec.set(i,(vec.get(i)+n)) ;
+            }
         }
-       }
-       if(f.equals("-")){
-           /**la sustraction*/
+        if(f.equals("-")){
+            /**la sustraction*/
 
-           for (int i=0;i<vec.size();i++) {
-               vec.set(i,(vec.get(i)-n)) ;
-           }
-       }
+            for (int i=0;i<vec.size();i++) {
+                vec.set(i,(vec.get(i)-n)) ;
+            }
+        }
         if(f.equals("*")){
-         /**la multiplication*/
+            /**la multiplication*/
 
             for (int i=0;i<vec.size();i++) {
                 vec.set(i,(vec.get(i)*n)) ;
             }
         }
         if(f.equals("/")){
-            /**la dévision*/
+            /**la devision*/
 
             for (int i=0;i<vec.size();i++) {
                 vec.set(i,(vec.get(i)/n)) ;
@@ -83,6 +86,8 @@ public class VectorHelper {
 
         return vec;
     }
+
+}
 
    
 
